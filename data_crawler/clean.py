f424255d19758d18eb14fb954eccaff1f5780d5b
Regan <gregatom99@gmail.com>
2023-05-26 11:01:59 +1000

    saving all the news data to single file; saving data to hugging face hub

--- data_crawler/clean.py
+++ data_crawler/clean.py
@@ -6,6 +6,8 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 csv_files = [f for f in os.listdir() if '.csv' in f and 'clean' not in f]
 print(csv_files)
+
+agg_data = pd.DataFrame(columns=['text'])
 for cf in tqdm(csv_files):
     data = pd.read_csv(cf)
     data = data[['text']]
@@ -20,5 +22,16 @@ for cf in tqdm(csv_files):
 
     data = data.apply(lambda x: removeNewsInitial(x),axis=1)
     ## end 
-
+    # print("Saving Individual file")
     data.to_csv("cleaned_{}".format(cf))
+    
+    ### Collective data
+    data['source'] = cf
+    agg_data = pd.concat([agg_data,data])
+    
+print(agg_data.shape)
+print("Saving Collective Data To File: NepCovNews.csv")
+agg_data.to_csv("NepCovNews.csv")
+
+# import datasets
+# datasets.Dataset.from_pandas(agg_data).push_to_hub("raygx/NepCovidHealthNews",token='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')
